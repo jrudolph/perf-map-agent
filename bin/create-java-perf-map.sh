ad40f776ee82c1a37ee830c6aea1af63cef9914d
Marc Bruggmann <marcbr@spotify.com>
2015-09-17 12:57:29 -0400

    Dont call sudo from the scripts.

--- bin/create-java-perf-map.sh
+++ bin/create-java-perf-map.sh
@@ -2,6 +2,11 @@
 set -e
 #set -x
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 CUR_DIR=`pwd`
 PID=$1
 OPTIONS=$2
@@ -17,6 +22,6 @@ fi
 [ -d $JAVA_HOME ] || JAVA_HOME=/etc/alternatives/java_sdk
 [ -d $JAVA_HOME ] || (echo "JAVA_HOME directory at '$JAVA_HOME' does not exist." && false)
 
-sudo rm $PERF_MAP_FILE -f
+rm $PERF_MAP_FILE -f
 (cd $PERF_MAP_DIR/out && java -cp $ATTACH_JAR_PATH:$JAVA_HOME/lib/tools.jar net.virtualvoid.perf.AttachOnce $PID $OPTIONS)
-sudo chown root:root $PERF_MAP_FILE
+chown root:root $PERF_MAP_FILE
