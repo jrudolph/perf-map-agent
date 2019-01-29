f7e7564d3d1059dcafd9e8ea9f8ff27369f92e73
Iori Yoneji <yoneji_i@worksap.co.jp>
2019-01-29 16:33:17 +0900

    check sudo command existence

--- bin/sudo-wrap.sh
+++ bin/sudo-wrap.sh
@@ -2,7 +2,13 @@
 
 function runasroot() {
     if [[ "$EUID" -ne 0 ]]; then
-        sudo $@
+        command -v sudo
+        if [[ $? -eq 0 ]]; then
+            sudo $@
+        else
+            echo "sudo is missing"
+            exit 1
+        fi
     else
         $@
     fi
