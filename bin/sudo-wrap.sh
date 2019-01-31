5514e3777dc36402b88eaffa829cc126612544fd
Iori Yoneji <yoneji_i@worksap.co.jp>
2019-01-31 21:42:12 +0900

    add -u option workaround

--- bin/sudo-wrap.sh
+++ bin/sudo-wrap.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 function runasroot() {
-    if [[ "$EUID" -ne 0 ]]; then
+    WANT_UID=0
+    if [[ "$1" == "-u" ]]; then
+        WANT_UID=$2
+    fi
+
+    if [[ "$EUID" -ne "${WANT_UID}" ]]; then
         command -v sudo
         if [[ $? -eq 0 ]]; then
             sudo $@
