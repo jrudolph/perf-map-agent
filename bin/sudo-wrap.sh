36e92a5d5c62d0bd38740bb10cb4314a191c5628
Iori Yoneji <yoneji_i@worksap.co.jp>
2019-02-01 12:35:44 +0900

    fix sudo-wrap

--- bin/sudo-wrap.sh
+++ bin/sudo-wrap.sh
@@ -4,12 +4,18 @@ function runasroot() {
     WANT_UID=0
     if [[ "$1" == "-u" ]]; then
         WANT_UID=$2
+        shift 2
     fi
 
-    if [[ "$EUID" -ne "${WANT_UID}" ]]; then
+    if [[ "$1" == "-g" ]]; then
+        WANT_GID=$2
+        shift 2
+    fi
+
+    if [[ "\#$EUID" == "${WANT_UID}" || "\#$EGID" == "${WANT_GID}" ]]; then
         command -v sudo
         if [[ $? -eq 0 ]]; then
-            sudo $@
+            sudo -u ${WANT_UID} -g ${WANT_GID} $@
         else
             echo "sudo is missing"
             exit 1
